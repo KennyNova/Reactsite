57dc9d942aab61dab40f2f68ec8e990be55ce5e6
NavidMadani <navidmadani4598@gmail.com>
2020-08-14 22:18:39 -0400

    projects page card fix

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,14 +5,26 @@ import { Card, CardActions, CardTitle, Button, CardMenu, CardText, IconButton } 
 class Projects extends Component {
     render() {
         return (
-            <div className="Projects" style={{position: 'fixed', margin: "5em"}}>
-                <Card  shadow={0} style={{width: '512px', margin: 'auto'}}>
-                    <CardTitle className="Card" style={{color: '#fff', height: '176px'}}>Pandam Tests</CardTitle>
+            <div style={{position: 'fixed', margin: "5em"}}>
+                <Card shadow={0} style={{width:'512px'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:'url(https://kinsta.com/wp-content/uploads/2018/04/what-is-github-1-1-1024x512.png) center / cover'}}>Pandam Tests</CardTitle>
                         <CardText >
                             Cypress test files for testing Pandams ui
                         </CardText>
                     <CardActions border>
                         <Button colored href="https://github.com/NavidMadani/pandam-tests" target="_blank">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card  shadow={0} style={{width: '512px', margin: 'auto'}}>
+                    <CardTitle className="Card" style={{color: '#fff', height: '176px', background:'url(https://kinsta.com/wp-content/uploads/2018/04/what-is-github-1-1-1024x512.png) center / cover'}}>Cypress</CardTitle>
+                        <CardText >
+                            First time trying out Cypress
+                        </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/NavidMadani/cypress" target="_blank">Github</Button>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share" />
